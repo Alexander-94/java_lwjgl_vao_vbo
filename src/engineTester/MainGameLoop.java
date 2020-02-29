de7e590a40b02e653db154cb3057b7087beefff2
Александр Юшкевич <alexandery41@gmail.com>
2020-02-29 17:54:27 +0200

    Tut 17 blendMap Terrain

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -25,6 +25,8 @@ import renderEngine.Loader;
 import renderEngine.MasterRenderer;
 import terrains.Terrain;
 import textures.ModelTexture;
+import textures.TerrainTexture;
+import textures.TerrainTexturePack;
 
 public class MainGameLoop {
 
@@ -67,9 +69,17 @@ public class MainGameLoop {
     Entity gentity = new Entity(grass, new Vector3f(0, 0, -10), 0, 0, 0, 1);
 
     //--terrain---------------------------------
-    Terrain terrain = new Terrain(0, -1, loader, new ModelTexture(loader.loadTexture("grass")));
-    Terrain terrain2 = new Terrain(-1, -1, loader, new ModelTexture(loader.loadTexture("grass")));
+    TerrainTexture backgroundTexture = new TerrainTexture(loader.loadTexture("grassy"));
+    TerrainTexture rTexture = new TerrainTexture(loader.loadTexture("dirt"));
+    TerrainTexture gTexture = new TerrainTexture(loader.loadTexture("pinkFlowers"));
+    TerrainTexture bTexture = new TerrainTexture(loader.loadTexture("path"));
 
+    TerrainTexturePack texturePack = new TerrainTexturePack(backgroundTexture, rTexture, gTexture,
+        bTexture);
+    TerrainTexture blendMap = new TerrainTexture(loader.loadTexture("blendMap"));
+
+    Terrain terrain = new Terrain(0, -1, loader, texturePack, blendMap);
+    Terrain terrain2 = new Terrain(-1, -1, loader, texturePack, blendMap);
     Camera camera = new Camera();
     MasterRenderer renderer = new MasterRenderer();
 
